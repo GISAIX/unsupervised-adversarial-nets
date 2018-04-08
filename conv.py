9260d58e4710b568307e765d4729cb2b58dd11a4
dongqixu <dqxu@cse.cuhk.edu.hk>
2018-04-08 14:36:58 +0800

    Coding style

--- conv.py
+++ conv.py
@@ -19,11 +19,11 @@ def conv_bn_relu(inputs, output_channels, kernel_size, stride, is_training, name
                  padding='same', use_bias=False, dilation=1):
     with tf.variable_scope(name_or_scope=name):
         conv = conv3d(inputs, output_channels, kernel_size, stride, padding=padding,
-                      use_bias=use_bias, name=name+'_conv', dilation=dilation)
+                      use_bias=use_bias, name=name + '_conv', dilation=dilation)
         bn = tf.contrib.layers.batch_norm(
             inputs=conv, decay=0.9, scale=True, epsilon=1e-5,
-            updates_collections=None, is_training=is_training, scope=name+'_batch_norm')
-        relu = tf.nn.relu(features=bn, name=name+'_relu')
+            updates_collections=None, is_training=is_training, scope=name + '_batch_norm')
+        relu = tf.nn.relu(features=bn, name=name + '_relu')
     return relu
 
 
@@ -32,9 +32,9 @@ def transform_layer(inputs, bottleneck_d, is_training, name, padding='same',
                     use_bias=False, dilation=1, stride=1):
     with tf.variable_scope(name_or_scope=name):
         layer_0 = conv_bn_relu(inputs, bottleneck_d, kernel_size=1, stride=1, is_training=is_training,
-                               name=name+'_i', padding=padding, use_bias=use_bias, dilation=dilation)
+                               name=name + '_i', padding=padding, use_bias=use_bias, dilation=dilation)
         layer_1 = conv_bn_relu(layer_0, bottleneck_d, kernel_size=3, stride=stride, is_training=is_training,
-                               name=name+'_ii', padding=padding, use_bias=use_bias, dilation=dilation)
+                               name=name + '_ii', padding=padding, use_bias=use_bias, dilation=dilation)
         return layer_1
 
 
@@ -43,7 +43,7 @@ def split_layer(inputs, cardinality, bottleneck_d, is_training, name,
     with tf.variable_scope(name_or_scope=name):
         layers_group = list()
         for i in range(cardinality):
-            layer = transform_layer(inputs, bottleneck_d, is_training, name+'_g'+str(i),
+            layer = transform_layer(inputs, bottleneck_d, is_training, name + '_g' + str(i),
                                     padding, use_bias, dilation, stride=stride)
             layers_group.append(layer)
         concat_dimension = 4  # channels_last
@@ -54,10 +54,10 @@ def transition_layer(inputs, output_channels, is_training, name, padding='same',
                      use_bias=False, dilation=1):
     with tf.variable_scope(name_or_scope=name):
         conv = conv3d(inputs, output_channels, kernel_size=1, stride=1, padding=padding,
-                      use_bias=use_bias, name=name+'_iii_conv', dilation=dilation)
+                      use_bias=use_bias, name=name + '_iii_conv', dilation=dilation)
         bn = tf.contrib.layers.batch_norm(
             inputs=conv, decay=0.9, scale=True, epsilon=1e-5,
-            updates_collections=None, is_training=is_training, scope=name+'_iii_batch_norm')
+            updates_collections=None, is_training=is_training, scope=name + '_iii_batch_norm')
         return bn
 
 
@@ -69,13 +69,13 @@ def aggregated_conv(inputs, output_channels, cardinality, bottleneck_d, is_train
     if residual:
         if transition.shape != inputs.shape:
             extension = conv3d(inputs, output_channels, kernel_size=1, stride=stride, padding='same',
-                               use_bias=True, name=name+'_residual', dilation=1)
+                               use_bias=True, name=name + '_residual', dilation=1)
             out = transition + extension
         else:
             out = transition + inputs
     else:
         out = transition
-    relu = tf.nn.relu(features=out, name=name+'_iii_relu')
+    relu = tf.nn.relu(features=out, name=name + '_iii_relu')
     return relu
 
 
@@ -83,12 +83,12 @@ def deconv3d(inputs, output_channels, name='deconv'):
     # depth, height and width
     batch, in_depth, in_height, in_width, in_channels = [int(d) for d in inputs.get_shape()]
     dev_filter = tf.get_variable(
-        name=name+'/filter', shape=[4, 4, 4, output_channels, in_channels],
+        name=name + '/filter', shape=[4, 4, 4, output_channels, in_channels],
         dtype=tf.float32, initializer=tf.random_normal_initializer(mean=0.0, stddev=0.01),
         regularizer=slim.l2_regularizer(scale=0.0005))
     deconv = tf.nn.conv3d_transpose(
         value=inputs, filter=dev_filter,
-        output_shape=[batch, in_depth*2, in_height*2, in_width*2, output_channels],
+        output_shape=[batch, in_depth * 2, in_height * 2, in_width * 2, output_channels],
         strides=[1, 2, 2, 2, 1], padding='SAME', data_format='NDHWC', name=name)
     '''Further adjustment in strides and filter shape'''
     return deconv
@@ -96,9 +96,9 @@ def deconv3d(inputs, output_channels, name='deconv'):
 
 def deconv_bn_relu(inputs, output_channels, is_training, name):
     with tf.variable_scope(name):
-        deconv = deconv3d(inputs=inputs, output_channels=output_channels, name=name+'_deconv')
+        deconv = deconv3d(inputs=inputs, output_channels=output_channels, name=name + '_deconv')
         bn = tf.contrib.layers.batch_norm(inputs=deconv, decay=0.9, scale=True, epsilon=1e-5,
                                           updates_collections=None, is_training=is_training,
-                                          scope=name+'_batch_norm')
-        relu = tf.nn.relu(features=bn, name=name+'_relu')
+                                          scope=name + '_batch_norm')
+        relu = tf.nn.relu(features=bn, name=name + '_relu')
     return relu
