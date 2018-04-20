5aa34e78d49694774d0f299a318879154e57eb16
dongqixu <dqxu@cse.cuhk.edu.hk>
2018-04-20 23:40:45 +0800

    [Feature] weighted loss with normalization

--- lossfun.py
+++ lossfun.py
@@ -23,17 +23,17 @@ def cross_entropy_loss(prediction, label, class_num):
     softmax_prediction = tf.nn.softmax(logits=prediction)
     ground_truth = tf.one_hot(indices=label, depth=class_num)
     loss = 0
-    # ratio = 1e-5
-    unique = len(tf.unique(label))
+    ratio = 1e-5
+    # unique = len(tf.unique(label))
     for i in range(class_num):
         i_prediction = softmax_prediction[:, :, :, :, i]
         i_ground_truth = ground_truth[:, :, :, :, i]
         # adjusted weight
         weight = tf.reduce_sum(i_ground_truth) / tf.reduce_sum(ground_truth)
-        loss -= tf.reduce_mean((1 / (weight + 1e-5) - 1) * i_ground_truth * tf.log(
+        loss -= tf.reduce_mean((1 - weight) * i_ground_truth * tf.log(
             tf.clip_by_value(t=i_prediction, clip_value_min=0.005, clip_value_max=1)))
-        # ratio += (1 - weight) * weight
-    return loss / (unique - 1)
+        ratio += (1 - weight) * weight
+    return loss / ratio  # (unique - 1) /
 
 
 def discriminative_loss(prediction, label, class_num=2):
